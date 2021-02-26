30f3f1254c79e29b82ef1864ecf351f38683d0b3
Juliana Simão <78971580+Julianajjss@users.noreply.github.com>
2021-02-26 13:14:21 -0300

    Add files via upload

---
+++ contas.py
@@ -0,0 +1,10 @@
+some = 1024+2048
+multiplique = 1024*2048
+divida = 2048/1024
+subtraia = 1024-2048
+print (some)
+print("multiplicacão: " ,multiplique)
+print (divida)
+print (subtraia)
+print (f"some: {some} \nmultiplique: {multiplique}")
+print (f"divida: {divida} \nsubtraia: {subtraia}")
